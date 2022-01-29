4319e4c16aa6a6ef00c2b3573d5e409122af63b4
yanrivera19 <yan.rivera.6@gmail.com>
2022-01-28 19:23:06 -0800

    deleted secret key from github repo

--- src/credentials/spotify.js
+++ src/credentials/spotify.js
@@ -1,4 +1,4 @@
 
-export const CLIENT_ID = "2a2ea7799f724d39a01a56ee873a34ba";
-export const CLIENT_SECRET = "3c9bbec7fbb84915a60066964b1989bc";
+export const CLIENT_ID = ;
+export const CLIENT_SECRET = ;
 
