4bf748be8e2fb50d0655282aea69cdac1f268318
yanrivera19 <yan.rivera.6@gmail.com>
2022-03-24 15:39:00 -0700

    removed unused folder

--- src/tests/Search.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen, fireEvent } from "@testing-library/react";
-import Search from "../components/Search";
-
-describe("Search component", () => {
-	it("should render Welcome if seachedTerm state is equal to empty string", () => {
-		const { queryByText } = render(<Search />);
-		const welcome = queryByText("Welcome");
-	});
-});
